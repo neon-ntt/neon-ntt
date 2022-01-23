8941358cc8826bb541ec3295d7cae50ac226d15d
vincentvbh <vincentvbh7@gmail.com>
2022-01-23 10:21:08 +0000

    add script for copying everything

---
+++ cp2supercop.sh
@@ -0,0 +1,19 @@
+
+SUPERCOP_PATH=~/supercop-20210604
+
+cp -rL kyber512/scheme $SUPERCOP_PATH/crypto_kem/kyber512/
+cp -rL kyber768/scheme $SUPERCOP_PATH/crypto_kem/kyber768/
+cp -rL kyber1024/scheme $SUPERCOP_PATH/crypto_kem/kyber1024/
+
+
+
+cp -rL lightsaber/scheme $SUPERCOP_PATH/crypto_kem/lightsaber/
+cp -rL saber/scheme $SUPERCOP_PATH/crypto_kem/saber/
+cp -rL firesaber/scheme $SUPERCOP_PATH/crypto_kem/firesaber/
+
+
+
+cp -rL dilithium2/scheme $SUPERCOP_PATH/crypto_sign/dilithium2/
+cp -rL dilithium3/scheme $SUPERCOP_PATH/crypto_sign/dilithium3/
+cp -rL dilithium5/scheme $SUPERCOP_PATH/crypto_sign/dilithium5/
+
