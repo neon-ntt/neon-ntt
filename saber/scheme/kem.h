734f0bfba6b511d0d03a02f52d49d0c224da6427
vincentvbh <vincentvbh7@gmail.com>
2023-04-22 14:21:23 +0200

    more clarification

--- saber/scheme/kem.h
+++ saber/scheme/kem.h
@@ -1,9 +1,9 @@
 /*=============================================================================
-This file has been adapted from the implementation 
-(available at, Public Domain https://github.com/KULeuven-COSIC/SABER) 
+This file has been adapted from the implementation
+(available at, Public Domain https://github.com/KULeuven-COSIC/SABER)
 of "Saber: Module-LWR based key exchange, CPA-secure encryption and CCA-secure KEM"
 by : Jan-Pieter D'Anvers, Angshuman Karmakar, Sujoy Sinha Roy, and Frederik Vercauteren
-Jose Maria Bermudo Mera, Michiel Van Beirendonck, Andrea Basso. 
+Jose Maria Bermudo Mera, Michiel Van Beirendonck, Andrea Basso.
 =============================================================================*/
 
 
