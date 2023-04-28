68720e8fef1204870ddb685f84c86378994ae8a0
vincentvbh <vincentvbh7@gmail.com>
2023-04-28 17:50:22 +0200

    swap arg

--- replaceWcommon.sh
+++ replaceWcommon.sh
@@ -1,28 +1,28 @@
 
 rmln () {
 
-rm -f $1
-ln -s $2 $1
+rm -f $2
+ln -s $1 $2
 
 }
 
 for x in dilithium2 dilithium3 dilithium5 kyber512 kyber768 kyber1024 lightsaber saber firesaber
 do
-    rmln $x/scheme/feat.S ../../common/feat.S
-    rmln $x/scheme/fips202.h ../../common/fips202.h
-    rmln $x/scheme/fips202.c ../../common/fips202.c
-    rmln $x/scheme/fips202x2.h ../../common/fips202x2.h
-    rmln $x/scheme/fips202x2.c ../../common/fips202x2.c
-    rmln $x/m1_benchmarks/m1cycles.h ../../common/m1cycles.h
-    rmln $x/m1_benchmarks/m1cycles.c ../../common/m1cycles.c
-    rmln $x/m1_benchmarks/feat.S ../../common/feat.S
-    rmln $x/m1_benchmarks/fips202.h ../../common/fips202.h
-    rmln $x/m1_benchmarks/fips202.c ../../common/fips202.c
-    rmln $x/m1_benchmarks/fips202x2.h ../../common/fips202x2.h
-    rmln $x/m1_benchmarks/fips202x2.c ../../common/fips202x2.c
-    rmln $x/m1_benchmarks/randombytes.h ../../common/randombytes.h
-    rmln $x/m1_benchmarks/randombytes.c ../../common/randombytes.c
-    rmln $x/microbenchmarks/hal.h ../../common/hal.h
-    rmln $x/microbenchmarks/hal.c ../../common/hal.c
+    rmln ../../common/feat.S $x/scheme/feat.S
+    rmln ../../common/fips202.h $x/scheme/fips202.h
+    rmln ../../common/fips202.c $x/scheme/fips202.c
+    rmln ../../common/fips202x2.h $x/scheme/fips202x2.h
+    rmln ../../common/fips202x2.c $x/scheme/fips202x2.c
+    rmln ../../common/m1cycles.h $x/m1_benchmarks/m1cycles.h
+    rmln ../../common/m1cycles.c $x/m1_benchmarks/m1cycles.c
+    rmln ../../common/feat.S $x/m1_benchmarks/feat.S
+    rmln ../../common/fips202.h $x/m1_benchmarks/fips202.h
+    rmln ../../common/fips202.c $x/m1_benchmarks/fips202.c
+    rmln ../../common/fips202x2.h $x/m1_benchmarks/fips202x2.h
+    rmln ../../common/fips202x2.c $x/m1_benchmarks/fips202x2.c
+    rmln ../../common/randombytes.h $x/m1_benchmarks/randombytes.h
+    rmln ../../common/randombytes.c $x/m1_benchmarks/randombytes.c
+    rmln ../../common/hal.h $x/microbenchmarks/hal.h
+    rmln ../../common/hal.c $x/microbenchmarks/hal.c
 done
 
