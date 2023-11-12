a1574556de151ce7056f88536fd0ecfe6cc79284
vincentvbh <vincentvbh7@gmail.com>
2023-11-12 14:38:32 +0100

    add name spacing

--- dilithium3/scheme/rounding.h
+++ dilithium3/scheme/rounding.h
@@ -10,12 +10,16 @@
 #include "params.h"
 #include <stdint.h>
 
+#define power2round DILITHIUM_NAMESPACE(power2round)
 int32_t power2round(int32_t *a0, int32_t a);
 
+#define decompose DILITHIUM_NAMESPACE(decompose)
 int32_t decompose(int32_t *a0, int32_t a);
 
+#define make_hint DILITHIUM_NAMESPACE(make_hint)
 unsigned int make_hint(int32_t a0, int32_t a1);
 
+#define use_hint DILITHIUM_NAMESPACE(use_hint)
 int32_t use_hint(int32_t a, unsigned int hint);
 
 #endif
--- dilithium3/scheme/symmetric.h
+++ dilithium3/scheme/symmetric.h
@@ -40,21 +40,25 @@
 typedef shake128incctx stream128_state;
 typedef shake256incctx stream256_state;
 
+#define dilithium_shake128_stream_init DILITHIUM_NAMESPACE(dilithium_shake128_stream_init)
 void dilithium_shake128_stream_init(shake128incctx *state,
                                     const uint8_t seed[SEEDBYTES],
                                     uint16_t nonce);
 
+#define dilithium_shake256_stream_init DILITHIUM_NAMESPACE(dilithium_shake256_stream_init)
 void dilithium_shake256_stream_init(shake256incctx *state,
                                     const uint8_t seed[CRHBYTES],
                                     uint16_t nonce);
 
+#define dilithium_shake128x2_stream_init DILITHIUM_NAMESPACE(dilithium_shake128x2_stream_init)
 void dilithium_shake128x2_stream_init(keccakx2_state *state,
                                       const uint8_t seed[SEEDBYTES],
                                       uint16_t nonce1, uint16_t nonce2);
-
+#define dilithium_shake256x2_stream_init DILITHIUM_NAMESPACE(dilithium_shake256x2_stream_init)
 void dilithium_shake256x2_stream_init(keccakx2_state *state,
                                       const uint8_t seed[CRHBYTES],
                                       uint16_t nonce1, uint16_t nonce2);
+
 
 #define STREAM128_BLOCKBYTES SHAKE128_RATE
 #define STREAM256_BLOCKBYTES SHAKE256_RATE
