b5b11932ab31f76644019718c748ac9266dff997
vincentvbh <vincentvbh7@gmail.com>
2024-10-09 15:32:00 +0200

    update ref

--- kyber1024/ref/verify.h
+++ kyber1024/ref/verify.h
@@ -1,11 +1,1 @@
-#ifndef VERIFY_H
-#define VERIFY_H
-#include "params.h"
-#include <stddef.h>
-#include <stdint.h>
-
-int verify(const uint8_t *a, const uint8_t *b, size_t len);
-
-void cmov(uint8_t *r, const uint8_t *x, size_t len, uint8_t b);
-
-#endif
+../../kyber768/ref/verify.h
--- kyber512/ref/params.h
+++ kyber512/ref/params.h
@@ -1,20 +1,44 @@
 #ifndef PARAMS_H
 #define PARAMS_H
 
+#ifndef KYBER_K
+#define KYBER_K 2	/* Change this for different security strengths */
+#endif
+
+
 /* Don't change parameters below this line */
+#if   (KYBER_K == 2)
+#define KYBER_NAMESPACE(s) pqcrystals_kyber512_ref_##s
+#elif (KYBER_K == 3)
+#define KYBER_NAMESPACE(s) pqcrystals_kyber768_ref_##s
+#elif (KYBER_K == 4)
+#define KYBER_NAMESPACE(s) pqcrystals_kyber1024_ref_##s
+#else
+#error "KYBER_K must be in {2,3,4}"
+#endif
+
 #define KYBER_N 256
 #define KYBER_Q 3329
 
 #define KYBER_SYMBYTES 32   /* size in bytes of hashes, and seeds */
 #define KYBER_SSBYTES  32   /* size in bytes of shared key */
 
-#define KYBER_POLYBYTES     384
-#define KYBER_POLYVECBYTES  (KYBER_K * KYBER_POLYBYTES)
+#define KYBER_POLYBYTES		384
+#define KYBER_POLYVECBYTES	(KYBER_K * KYBER_POLYBYTES)
 
-#define KYBER_K 2
+#if KYBER_K == 2
 #define KYBER_ETA1 3
 #define KYBER_POLYCOMPRESSEDBYTES    128
 #define KYBER_POLYVECCOMPRESSEDBYTES (KYBER_K * 320)
+#elif KYBER_K == 3
+#define KYBER_ETA1 2
+#define KYBER_POLYCOMPRESSEDBYTES    128
+#define KYBER_POLYVECCOMPRESSEDBYTES (KYBER_K * 320)
+#elif KYBER_K == 4
+#define KYBER_ETA1 2
+#define KYBER_POLYCOMPRESSEDBYTES    160
+#define KYBER_POLYVECCOMPRESSEDBYTES (KYBER_K * 352)
+#endif
 
 #define KYBER_ETA2 2
 
