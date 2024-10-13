08b92b8f1d6fea1598cbbd7aee55855538d4ee6d
vincentvbh <vincentvbh7@gmail.com>
2024-10-13 20:11:40 +0200

    mv wraper to cycles.h

--- common/cycles.h
+++ common/cycles.h
@@ -1,10 +1,57 @@
 #ifndef CYCLES_H
 #define CYCLES_H
 
+// Define either __AVERAGE__ or __MEDIAN__ before including this header
+
 #include <stdint.h>
 
 void init_counter(void);
 uint64_t get_cycle(void);
+
+#ifdef __AVERAGE__
+
+#define LOOP_INIT(__clock0, __clock1) { \
+    __clock0 = get_cycle(); \
+}
+#define LOOP_TAIL(__f_string, records, __clock0, __clock1) { \
+    __clock1 = get_cycle(); \
+    printf(__f_string " average cycles: %lld\n", (__clock1 - __clock0) / NTESTS); \
+}
+#define BODY_INIT(__clock0, __clock1) {}
+#define BODY_TAIL(records, __clock0, __clock1) {}
+
+#elif defined(__MEDIAN__)
+
+#include <stdlib.h>
+
+static int cmp_uint64(const void *a, const void *b){
+    return ((*((const uint64_t*)a)) - ((*((const uint64_t*)b))));
+}
+
+#define LOOP_INIT(__clock0, __clock1) {}
+#define LOOP_TAIL(__f_string, records, __clock0, __clock1) { \
+    qsort(records, sizeof(uint64_t), NTESTS, cmp_uint64); \
+    printf(__f_string " median cycles: %lld\n", records[NTESTS >> 1]); \
+}
+#define BODY_INIT(__clock0, __clock1) { \
+    __clock0 = get_cycle(); \
+}
+#define BODY_TAIL(records, __clock0, __clock1) { \
+    __clock1 = get_cycle(); \
+    records[i] = __clock1 - __clock0; \
+}
+
+#endif
+
+#define WRAP_FUNC(__f_string, records, __clock0, __clock1, func){ \
+    LOOP_INIT(__clock0, __clock1); \
+    for(size_t i = 0; i < NTESTS; i++){ \
+        BODY_INIT(__clock0, __clock1); \
+        func; \
+        BODY_TAIL(records, __clock0, __clock1); \
+    } \
+    LOOP_TAIL(__f_string, records, __clock0, __clock1); \
+}
 
 #endif
 
--- kyber768/scheme/kyber_speed.c
+++ kyber768/scheme/kyber_speed.c
@@ -9,60 +9,15 @@
 #include "poly.h"
 #include "polyvec.h"
 #include "ntt.h"
+
+#define __AVERAGE__
+// #define __MEDIAN__
 #include "cycles.h"
 
 #define NTESTS 1000000
 
 uint64_t time0, time1;
 uint64_t cycles[NTESTS];
-
-#define __AVERAGE__
-// #define __MEDIAN__
-
-#ifdef __AVERAGE__
-
-#define LOOP_INIT(__clock0, __clock1) { \
-    __clock0 = get_cycle(); \
-}
-#define LOOP_TAIL(__f_string, records, __clock0, __clock1) { \
-    __clock1 = get_cycle(); \
-    printf(__f_string " average cycles: %lld\n", (__clock1 - __clock0) / NTESTS); \
-}
-#define BODY_INIT(__clock0, __clock1) {}
-#define BODY_TAIL(records, __clock0, __clock1) {}
-
-#elif defined(__MEDIAN__)
-
-#include <stdlib.h>
-
-static int cmp_uint64(const void *a, const void *b){
-    return ((*((const uint64_t*)a)) - ((*((const uint64_t*)b))));
-}
-
-#define LOOP_INIT(__clock0, __clock1) {}
-#define LOOP_TAIL(__f_string, records, __clock0, __clock1) { \
-    qsort(records, sizeof(uint64_t), NTESTS, cmp_uint64); \
-    printf(__f_string " median cycles: %lld\n", records[NTESTS >> 1]); \
-}
-#define BODY_INIT(__clock0, __clock1) { \
-    __clock0 = get_cycle(); \
-}
-#define BODY_TAIL(records, __clock0, __clock1) { \
-    __clock1 = get_cycle(); \
-    records[i] = __clock1 - __clock0; \
-}
-
-#endif
-
-#define WRAP_FUNC(__f_string, records, __clock0, __clock1, func){ \
-    LOOP_INIT(__clock0, __clock1); \
-    for(size_t i = 0; i < NTESTS; i++){ \
-        BODY_INIT(__clock0, __clock1); \
-        func; \
-        BODY_TAIL(records, __clock0, __clock1); \
-    } \
-    LOOP_TAIL(__f_string, records, __clock0, __clock1); \
-}
 
 int main(void){
 
